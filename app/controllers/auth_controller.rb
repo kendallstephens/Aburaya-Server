5076376872e6be9cacbaa9bb09cfe80a500c3ed0
kendallstephens <kendoll.stephens@gmail.com>
2020-11-24 15:43:48 -0800

    before action added in application controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-    # skip_before_action :authorized, only: [:login]
+    skip_before_action :authorized, only: [:login]
 
     def login
         user = User.find_by(email: params[:email])
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    # skip_before_action :authorized, only: [:create]
+    skip_before_action :authorized, only: [:index, :show]
 
     def index
         items = Item.all

99d3b3b2be241f88561829b11482d48b3d047cb5
kendallstephens <kendoll.stephens@gmail.com>
2020-11-27 18:30:07 -0800

    Order & Order Item Controllers updated

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,25 @@
 class OrderItemsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show, :create]
+
+    def index 
+        order_items = OrderItem.all 
+        render json: order_items
+    end
+
+    def show
+        order_item = OrderItem.find(params[:id])
+        render json: order_item
+    end
+
+
+    def create
+        order_item = OrderItem.create(order_item_params)
+        render json: order_item
+    end
+
+    private
+
+    def order_item_params
+        params.permit(:item_id, :order_id)
+    end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,43 @@
 class OrdersController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show, :create]
+
+    def index 
+        orders = Order.all 
+        render json: orders
+    end
+
+    def show
+        order = Order.find(params[:id])
+        render json: order
+    end 
+
+
+    def create
+        order = Order.create(order_params)
+
+        if order.valid?
+            render json: order
+        else 
+            render json: {error: "Your order did not go through, please try again"}
+        end 
+    end
+
+    def update 
+        order = Order.find(params[:id])
+        order.update(order_params)
+
+        if order.valid?
+            render json: order
+        else 
+            render json: {error: "Your order was not updated, please try again"}
+        end 
+    end
+
+    private 
+
+    def order_params
+        params.permit(:user_id, :paid)
+    end 
+
+
 end

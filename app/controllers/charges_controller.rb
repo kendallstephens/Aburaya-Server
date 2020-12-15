28b6c2c4d889a0d81be3029dd7203250d63e461e
kendallstephens <kendoll.stephens@gmail.com>
2020-12-15 15:08:33 -0800

    small update

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,32 @@
 class ChargesController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
+   
+
+
+
     def create
-      Stripe.api_key = ENV['stripe_api_key']
+      byebug
+   
+      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
       find_items = User.all.filter{|user| user.id === params[:checkout_user_id] }
       items = find_items.map{ |user| user.items}
-      amount = items.map{|item| item.price}.sum * 100
+      amount = 5 * 100
+      payment_method = params[:payment_method]
      
       intent = Stripe::PaymentIntent.create({
+          payment_method: payment_method,
           amount: amount.to_i,
           currency: 'usd',
+          confirmation_method: 'automatic',
           metadata: {integration_check: 'accept_a_payment'},
         })
         render json: {client_secret: intent.client_secret}
-  end
+      end
+
+
+  
+       
+
 end
